eb8b682ae18110692ec4b3014a82177ac8aa8e00
Hezkiel Siregar <hezkielrivaldos@gmail.com>
2023-06-11 20:22:48 +0700

    + add assets
    + add assets

---
+++ src/assets/index.js
@@ -0,0 +1,227 @@
+//ICONS
+//Menu
+import MenuBurger from './icons/menus/menu_burger.svg'
+import MenuBtnArrow from './icons/menus/menu_btn_arrow.svg'
+import MenuArrowLeft from './icons/menus/menu_arrow_left.svg'
+import MenuClose from './icons/menus/menu_close.svg'
+import MenuHome from './icons/menus/menu_home.svg'
+import MenuHomeFilled from './icons/menus/menu_home_filled.svg'
+import MenuStoreManagement from './icons/menus/menu_store_management.svg'
+import MenuStoreManagementFilled from './icons/menus/menu_store_management_filled.svg'
+import MenuArticle from './icons/menus/menu_article.svg'
+import MenuArticleFilled from './icons/menus/menu_article_filled.svg'
+import MenuMessage from './icons/menus/menu_message.svg'
+import MenuMessageFilled from './icons/menus/menu_message_filled.svg'
+import MenuChevron from './icons/menus/menu_chevron.svg'
+import MenuSearch from './icons/menus/menu_search.svg'
+import MenuAccount from './icons/menus/menu_account.svg'
+import MenuChangePassword from './icons/menus/menu_change_password.svg'
+import MenuLogout from './icons/menus/menu_logout.svg'
+import MenuThreePoint from './icons/menus/menu_three_point.svg'
+import NotFound from './images/logos/not_found.png'
+import NotFoundLg from './images/logos/not_found_lg.png'
+
+//Noun
+import NounMail from './icons/nouns/noun_mail.svg'
+import NounEye from './icons/nouns/noun_eye.svg'
+import NounEyeSlash from './icons/nouns/noun_eye_slash.svg'
+import NounSuccess from './icons/nouns/noun_success.svg'
+import NounFile from './icons/nouns/noun_file.svg'
+import NounDot from './icons/nouns/noun_dot.svg'
+
+//IMAGES
+//Menu
+import MenuWelcomePeople from './images/menus/menu_welcome_people.png'
+import MenuEmptyItem from './images/menus/menu_empty_item.png'
+
+//Logo
+import LogoFacebook from './images/logos/logo_facebook.png'
+import LogoInstagram from './images/logos/logo_instagram.png'
+import LogoWhatsapp from './images/logos/logo_whatsapp.png'
+import LogoJakomkris from './images/logos/logo_jakomkris.png'
+import LogoLocation from  './images/logos/logo_location.png'
+import LogoYoutube from './images/logos/logo_youtube.png'
+import LogoEmail from './images/logos/logo_email.png'
+import Danger from './images/logos/danger.png'
+import Grow from './images/logos/grow.png'
+import Down from './images/logos/down.png'
+import Location from './images/logos/location.png'
+import Quake from './images/logos/quake.png'
+import Disaster from './images/logos/disaster.png'
+import Sky from './images/logos/sky.jpg'
+import SunnyCloudly from './images/logos/sunnyCloudly.png'
+import Drizzling from './images/logos/drizzling.png'
+import ModerateRain from './images/logos/moderateRain.png'
+import Cloudly from './images/logos/cloudly.png'
+import IconHeaderChevron from './images/logos/icon_header_chevron.png'
+import CaseCovid from './images/logos/caseCovid.png'
+import PositifCovid from './images/logos/positifCovid.png'
+import Die from './images/logos/die.png'
+import Baby from './images/logos/baby.png'
+import Young from './images/logos/young.png'
+import Worker from './images/logos/worker.png'
+import MiddleAge from './images/logos/middleAge.png'
+import Older from './images/logos/older.png'
+import OldAge from './images/logos/oldAge.png'
+import Vaksin from './images/logos/vaksin.png'
+import HumanResource from './images/logos/humanResource.png'
+import Officer from './images/logos/officer.png'
+import One from './images/logos/one.png'
+import Two from './images/logos/two.png'
+import ThunderRain from './images/logos/thundeRain.png'
+import HeavyRain from './images/logos/heavyRain.png'
+import Sunny from './images/logos/Sunny.png'
+import LogoMap from './images/logos/mapping.png'
+import LogoWebPortal from './images/logos/webPortal.png'
+import LogoChurch from './images/logos/church.png'
+import LogoData from './images/logos/data.png'
+import church from './images/logos/church.png'
+import menuMarker from './images/logos/marker.png'
+import menuMarkerFilled from './images/logos/markerFilled.png'
+import menuForum from './images/logos/forum.png'
+import menuForumFilled from './images/logos/forumFilled.png'
+import menuDashboard from './images/logos/dashboard.png'
+import menuDashboardFilled from './images/logos/dashboardFilled.png'
+import MenuChurch from './images/logos/menuChurch.png'
+import MenuChurchFilled from './images/logos/menuChurchFilled.png'
+import Faq from './images/logos/faq.png'
+import FaqFilled from './images/logos/faqFilled.png'
+import IconChurch from './images/logos/icon_church.png'
+import IconGrid from './images/logos/icon_grid.png'
+import Check from './images/logos/check.png'
+import Decline from './images/logos/decline.png'
+import LogoUKDW from './images/logos/logo_ukdw.png'
+import LogoGKJ from './images/logos/logo_gkj.png'
+
+
+//Backgrounds
+import BackgroundIndomaret from './images/backgrounds/bg_indomaret.png'
+import BackgroundContact from './images/backgrounds/bg_contact_us.jpg'
+
+//Icon
+import MarkerIcon from './images/logos/icon_marker.png'
+import PersonIcon from './images/logos/icon_person.png'
+
+
+//Thumbnail
+import DefaultState from './images/article/default_state.png'
+
+
+//Banner
+import Banner from './images/banner/banner.png'
+
+//TEAM
+import Nafarel from './images/logos/nafarel.jpg'
+import Desendo from './images/logos/desendo.jpg'
+import Ebentera from './images/logos/Ebentera.jpg'
+import Stevani from './images/logos/Stevani.jpeg'
+import Jesslyn from './images/logos/Jesslyn.jpeg'
+import Hezkiel from './images/logos/Hezkiel.jpg'
+
+
+export {
+    //Icons
+    MenuBurger,
+    MenuBtnArrow,
+    MenuHome,
+    MenuHomeFilled,
+    MenuStoreManagement,
+    MenuStoreManagementFilled,
+    MenuArticle,
+    church,
+    MenuArticleFilled,
+    MenuMessage,
+    MenuMessageFilled,
+    MenuChevron,
+    MenuSearch,
+    MenuAccount,
+    IconChurch,
+    IconGrid,
+    MenuChangePassword,
+    MenuLogout,
+    MenuThreePoint,
+    NounMail,
+    MenuArrowLeft,
+    MenuClose,
+    NounEye,
+    NounEyeSlash,
+    NounSuccess,
+    NounFile,
+    NounDot,
+    MarkerIcon,
+    PersonIcon,
+    NotFound,
+    NotFoundLg,
+
+    //Images
+    MenuWelcomePeople,
+    BackgroundIndomaret,
+    BackgroundContact,
+    MenuEmptyItem,
+    LogoFacebook,
+    LogoInstagram,
+    LogoWhatsapp,
+    LogoJakomkris,
+    LogoUKDW,
+    LogoGKJ,
+    LogoLocation,
+    LogoYoutube,
+    LogoEmail,
+    Danger,
+    LogoMap,
+    LogoWebPortal,
+    LogoChurch,
+    LogoData,
+    menuMarker,
+    menuMarkerFilled,
+    menuForum,
+    menuForumFilled,
+    menuDashboard,
+    menuDashboardFilled,
+    Faq,
+    FaqFilled,
+    MenuChurch,
+    MenuChurchFilled,
+    
+    //Thumbnail
+    DefaultState,
+
+    //Banner
+    Banner,
+    Grow,
+    Down,
+    Location,
+    Quake,
+    Disaster,
+    Sky,
+    SunnyCloudly,
+    Drizzling,
+    ModerateRain,
+    Cloudly,
+    IconHeaderChevron,
+    CaseCovid,
+    PositifCovid,
+    Die,
+    Baby,
+    Young,
+    Worker,
+    MiddleAge,
+    Older,
+    OldAge,
+    Vaksin,
+    HumanResource,
+    Officer,
+    One,
+    Two,
+    ThunderRain,
+    HeavyRain,
+    Sunny,
+    Check,
+    Decline,
+    Nafarel,
+    Desendo,
+    Ebentera,
+    Jesslyn,
+    Stevani,
+    Hezkiel
+}
